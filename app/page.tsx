174f188f7077dd5d587bca14cd74d83d5d7f196b
jordahn-funnell <jordahn.funnell@roamdigital.com>
2023-09-06 15:21:22 +1200

    extra bits

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,33 @@ export default function Home() {
   return (
     <main className="flex">
       <div className="stage-3d">
-        <div className="panel front-panel">Testing testing testing</div>
-        <div className="panel middle-panel">Testing</div>
-        <div className="panel rear-panel"></div>
+        <div className="panel front-left-panel">TESTING TESTING TESTING</div>
+        <div className="panel middle-left-panel">TESTING</div>
         <div className="left-wall-front"></div>
         <div className="left-wall-middle">
           <div className="panel">TESTING TESTING</div>
         </div>
+        <div className="panel rear-left-panel">TESTING TESTING TESTING</div>
         <div className="left-wall-back"></div>
-        <div className="center-panel text-white">Testing</div>
+        <div className="left-wall-back-2"></div>
+        <div className="left-wall-back-3"></div>
+        <div className="center-building">
+          <div className="m-1 w-9/10 h-24 bg-white"></div>
+          <div className="m-1 w-9/10 h-48 bg-red-500"></div>
+          <div className="m-1 w-9/10 h-32 bg-cyan-500"></div>
+          <div className="m-1 w-9/10 h-32 bg-green-500"></div>
+          <div className="m-1 w-9/10 h-48 bg-yellow-500"></div>
+        </div>
         <div className="right-wall-back"></div>
         <div className="right-wall-middle"></div>
+        <div className="panel front-right-panel">TESTING TESTING TESTING</div>
         <div className="right-wall-front">
-          <div className="panel">TESTING TESTING</div>
+          <div className="bg-white m-1">TESTING TESTING</div>
+          <div className="bg-white m-1 h-48">TESTING TESTING</div>
+          <div className="bg-white m-1 h-48">TESTING TESTING</div>
+          <div className="bg-white m-1">TESTING TESTING</div>
         </div>
       </div>
-      {/* <div className="stage-3d">
-        <div className="left-wall-back"></div>
-        <div className="right-wall-back"></div>
-        <div className="right-wall-middle"></div>
-        <div className="right-wall-front"></div>
-      </div> */}
     </main>
   );
 }
