7509c2e515c568948f55f3a4f783de88eef03702
jordahn-funnell <jordahn.funnell@roamdigital.com>
2023-09-13 17:38:21 +1200

    pre-componentizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex justify-center">
       <div className="stage-3d">
-        <div className="panel front-left-panel">TESTING TESTING TESTING</div>
-        <div className="panel middle-left-panel">TESTING</div>
         <div className="left-wall-front"></div>
-        <div className="left-wall-middle">
-          <div className="panel">TESTING TESTING</div>
-        </div>
-        <div className="panel rear-left-panel">TESTING TESTING TESTING</div>
+        <div className="left-wall-middle"></div>
         <div className="left-wall-back"></div>
-        <div className="left-wall-back-2"></div>
-        <div className="left-wall-back-3"></div>
         <div className="center-building">
           <div className="m-1 w-9/10 h-24 bg-white"></div>
           <div className="m-1 w-9/10 h-48 bg-red-500"></div>
@@ -26,12 +14,7 @@ export default function Home() {
         </div>
         <div className="right-wall-back"></div>
         <div className="right-wall-middle"></div>
-        <div className="panel front-right-panel">TESTING TESTING TESTING</div>
         <div className="right-wall-front">
-          <div className="bg-white m-1">TESTING TESTING</div>
-          <div className="bg-white m-1 h-48">TESTING TESTING</div>
-          <div className="bg-white m-1 h-48">TESTING TESTING</div>
-          <div className="bg-white m-1">TESTING TESTING</div>
         </div>
       </div>
     </main>
